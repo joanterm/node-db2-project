b587b0eb45cce11c19b57fcdc363bf4d9fc93ff7
joanterm <jo_joanarg@yahoo.com>
2022-08-17 08:12:02 -0400

    GET and GET by id work

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,14 +1,31 @@
 // DO YOUR MAGIC
 const express = require("express")
 const router = express.Router()
+const Cars = require("./cars-model")
 
 router.get("/", (req, res) => {
-    console.log("works")
+    Cars.getAll()
+    .then((result) => {
+        res.status(200).json(result)
+    })
+    .catch((err) => {
+        console.log(err);        
+    })
 })
 
-// router.get("/:id", (req, res) => {
-
-// })
+router.get("/:id", (req, res) => {
+    Cars.getById(req.params.id)
+    .then((result) => {
+        if(result == null) {
+            res.status(404).json({message: `car with id ${req.params.id} is not found`})
+            return
+        }
+        res.status(200).json(result)
+    })
+    .catch((err) => {
+        console.log(err);        
+    })
+})
 
 // router.post("/", (req, res) => {
 
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,20 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable("cars", tbl => {
+    tbl.increments()
+    tbl.text("vin", 20)
+      .notNullable()
+      .unique()
+    tbl.text("make", 20)
+      .notNullable()
+    tbl.text("model", 20)
+      .notNullable()
+    tbl.integer("mileage", 20)
+      .notNullable()
+    tbl.text("title", 20)
+    tbl.text("transmission", 20)
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists("cars")
 };
